8777d1ed6a213817653aee9479b69040c255653c
Kashfa <cyan82@hotmail.com>
2018-03-26 13:20:40 +0100

    created DatabaseManger

---
+++ app/src/main/java/com/example/kashfatahir/todolistappproject/DatabaseManager.java
@@ -0,0 +1,22 @@
+package com.example.kashfatahir.todolistappproject;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by kashfatahir on 26/03/2018.
+ */
+
+public class DatabaseManager extends SQLiteOpenHelper {
+
+    
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
